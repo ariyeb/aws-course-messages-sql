29363ee43eb0ee4fef7a34c4af9e80f981333d60
Ariye Bar-tov <ariyeb@gmail.com>
2021-03-10 20:47:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT;
+const sequelize = require("./db/sequelize");
+const messagesRouter = require('./routers/messageRouter');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(messagesRouter);
+
+sequelize.sync()
+    .then(() => app.listen(port, () => console.log("Server connectes, port:", port)))
+    .catch(err => console.log(err));
